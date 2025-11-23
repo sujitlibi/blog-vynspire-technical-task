c8e6e4e885c5f37982fce3ed598026ec0e036142
Sujit Kumar Libi <sujit_libi@hotmail.com>
2025-11-23 12:12:10 +0545

    feature: initialize tailwind css v4

--- src/pages/home/LandingPage.tsx
+++ src/pages/home/LandingPage.tsx
@@ -1,7 +1,7 @@
 const LandingPage = () => {
   return (
     <div>
-      <h1>This is Landing Page</h1>
+      <h1 className="text-3xl font-bold underline">This is Landing Page</h1>
     </div>
   );
 };
